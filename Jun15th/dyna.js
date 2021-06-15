ddef4fb46e11b9f2ddfae2043ccbd98c046e31a9
KAA5HU <k.neeladatta@gmail.com>
2021-06-15 12:04:03 +0530

    Data on web

---
+++ Jun15th/dyna.js
@@ -0,0 +1,47 @@
+const searchResultsEle = document.getElementById('searchResults');
+const inputEle = document.getElementById("input");
+
+let results = []
+
+function createAndAppend(each) {
+
+    let divEle = document.createElement("div");
+    divEle.classList.add()
+    console.log(each.name)
+
+    let nameEle = document.createElement("p")
+    nameEle.textContent = each.name
+    divEle.appendChild(nameEle)
+
+    let emailEle = document.createElement("p")
+    emailEle.textContent = each.email
+    divEle.appendChild(emailEle)
+
+    searchResultsEle.appendChild(divEle)
+}
+
+function displayResults(results) {
+    for (let each in results) {
+        createAndAppend(each)
+    }
+    
+}
+
+function getData(event) {
+            
+            let url = 'https://jsonplaceholder.typicode.com/users'
+            let options = {
+                method: "GET"
+            };
+            fetch(url, options)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(jsonData) {
+                    console.log(jsonData)
+                    results = jsonData
+                    displayResults()
+                })
+}
+
+inputEle.addEventListener("keyup", getData)
