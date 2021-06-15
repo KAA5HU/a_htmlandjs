3fcb728a846c5023ab3ecbe2944462fbf1bac81d
KAA5HU <k.neeladatta@gmail.com>
2021-06-15 12:28:42 +0530

    comments

--- Jun15th/dyna.js
+++ Jun15th/dyna.js
@@ -1,9 +1,12 @@
+//this is the comtainer for the search results elements
 const searchResultsEle = document.getElementById('searchResults');
 const inputEle = document.getElementById("input");
 
+//empty list
 let results = []
 
 function createAndAppend(each) {
+    //creating the each element using createelement method 
     let divEle = document.createElement("div");
     divEle.classList.add("each-person")
 
@@ -19,19 +22,20 @@ function createAndAppend(each) {
 }
 
 function displayResults(results) {
+    //data is stored in the results list
     for (let each of results) {
+        //creating the each results as saperate container 
+        //and appending to the main container
         createAndAppend(each)
         
     }
-    
-    
 }
-
+//
 function getData(event) {
             
             let url = 'https://jsonplaceholder.typicode.com/users'
             let options = {
-                method: "GET"
+                method: "GET"   //get method is used to retrive the data
             };
             fetch(url, options)
                 .then(function(response) {
@@ -43,5 +47,5 @@ function getData(event) {
                     displayResults(results)
                 })
 }
-
+//user action is required
 inputEle.addEventListener("keyup", getData)
