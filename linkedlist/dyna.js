ff738111a7ca95eeaae652558dd6394064868c60
KAA5HU <k.neeladatta@gmail.com>
2021-06-16 12:25:38 +0530

    adding more operations

--- linkedlist/dyna.js
+++ linkedlist/dyna.js
@@ -1,3 +1,4 @@
+//getting all the elements from the file index.html
 const linkedListEle = document.getElementById("linkedList");
 const inputElementEle = document.getElementById("inputElement");
 const onClickAddEle = document.getElementById("onClickAdd");
@@ -5,12 +6,13 @@ const onClickDelEle = document.getElementById("onClickDel");
 const onClickAddAtBegEle = document.getElementById('onClickAddAtBeg');
 const inputElementBegEle = document.getElementById('inputElementBeg');
 const onClickDelStartEle = document.getElementById('onClickDelStart');
+const removeNodeEle = document.getElementById('removeNode');
+const ElementRemoveClickEle = document.getElementById('ElementRemoveClick');
 
-var count = 0;
-
+//Here every node and arrow is creating but its not printing
+//its just returning
 function createAndAppend(number) {
-    let nodeId = "node" + count
-    count = count + 1
+    let nodeId = "node" + number
     let imgAndNode = document.createElement("div");
     imgAndNode.id = nodeId;
     imgAndNode.classList.add("a")
@@ -20,39 +22,66 @@ function createAndAppend(number) {
 
     let numberEle = document.createElement("p");
     numberEle.classList.add("number");
-    numberEle.textContent = number;
+    numberEle.textContent = number; //value
     nodeCon.appendChild(numberEle);
 
     imgAndNode.appendChild(nodeCon)
 
-    let imgCon = document.createElement("i")
+    let imgCon = document.createElement("i") //arrow image
     imgCon.classList.add("fas",  "fa-long-arrow-alt-right", "arrow")
     imgAndNode.appendChild(imgCon)
 
     return imgAndNode
 }
 
-
+//returning the node and array 
+//and appending to the linkedlListEle
 function addEle() {
     let number = inputElementEle.value;
     linkedListEle.appendChild(createAndAppend(number))
+    inputElementEle.value = ""
 }
-
+//Used the Dom Elements to perform the operations directly
 function delEle() {
 
     linkedListEle.lastChild.remove()
 }
 
+//function to add elements at the beggining
+//for that prepend method is used
 function addBegEle() {
     let number = inputElementBegEle.value
     linkedListEle.prepend(createAndAppend(number))
+    inputElementBegEle.value = ""
 }
 
+//function is used to remove the start node 
+//from the linkedListEle
 function removeStart() {
     linkedListEle.firstChild.remove()
 }
 
+//function used to remove the an element which user want to remove
+function removeAnyEle() {
+    number = removeNodeEle.value
+    let count = document.getElementsByTagName('i').length
+    if (count == 0) {
+        alert("List is empty") //if list is empty
+    }
+    else{
+    for (var i = 0; i<count; i++) {
+        if(document.getElementById("node" + number).textContent == number) {
+            document.getElementById("node" + number).remove()
+        }
+    }
+    removeNodeEle.value = ""
+}
+}
+
+
+//This are the event listeners
 onClickAddEle.addEventListener('click', addEle)
 onClickDelEle.addEventListener('click', delEle)
 onClickAddAtBegEle.addEventListener('click', addBegEle)
 onClickDelStartEle.addEventListener('click', removeStart)
+ElementRemoveClickEle.addEventListener('click', removeAnyEle)
